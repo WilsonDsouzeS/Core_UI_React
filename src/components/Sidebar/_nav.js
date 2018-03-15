263c556aa5a01639be2eb20aa927f1fdded5437e
Wilson Stephen <wilsons@pricoltech.com>
2018-03-15 11:17:41 +0530

    React Sample Application..

---
+++ src/components/Sidebar/_nav.js
@@ -0,0 +1,14 @@
+export default {
+  items: [
+    {
+      name: 'Dashboard',
+      url: '/dashboard',
+      icon: 'icon-speedometer',
+    },
+    {
+      name: 'Charts',
+      url: '/charts',
+      icon: 'icon-pie-chart'
+    }
+  ]
+};
