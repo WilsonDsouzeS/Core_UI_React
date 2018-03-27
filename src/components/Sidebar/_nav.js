f6ae0098ca594d71b431f5af40f6b2ba7a12c87a
Wilson Stephen <wilsons@pricoltech.com>
2018-03-27 18:01:36 +0530

    MQTT pub and sub included and hides chart and mqtt test menu

--- src/components/Sidebar/_nav.js
+++ src/components/Sidebar/_nav.js
@@ -5,20 +5,20 @@ export default {
       url: '/dashboard',
       icon: 'icon-speedometer',
     },
-    {
+  /*{
       name: 'Charts',
       url: '/charts',
       icon: 'icon-pie-chart'
-    },
+    }, */
     {
       name: 'Machines',
       url: '/machines',
       icon: 'icon-puzzle'
-    },
+    }/* ,
     {
       name: 'MQTT-Test',
       url: '/mqtt_test',
       icon: 'icon-puzzle'
-    }
+    } */
   ]
 };
