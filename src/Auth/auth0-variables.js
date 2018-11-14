1784e938bc573fc406c62bb07f53cfd9e4c7f929
Wilson Stephen <wilsons@pricoltech.com>
2018-11-14 14:44:24 +0530

    Auth0 cvariable file modified

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
   domain: 'wils.auth0.com',
   clientId: 'DPxoTRJR2dBzhKdJOlbJL_zXvm_Ct9TX',
-  //callbackUrl: 'http://localhost:8082/#/dashboard'   // Local
-  callbackUrl: 'http://oee-demo-integrated.s3-website-us-east-1.amazonaws.com/#/dashboard'  // S3 - oee-demo-integrated - Running
+  callbackUrl: 'http://localhost:8082/#/dashboard'   // Local
+  //callbackUrl: 'http://oee-demo-integrated.s3-website-us-east-1.amazonaws.com/#/dashboard'  // S3 - oee-demo-integrated - Running
 }
